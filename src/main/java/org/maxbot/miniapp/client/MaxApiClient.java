fa750c5540446f7bdd0d52534de099ddb64d49cd
alex <4.alex.skorikov@gmail.com>
2026-07-07 08:18:22 +0300

    Тестовая реализация чата - логирование запросов

--- src/main/java/org/maxbot/miniapp/client/MaxApiClient.java
+++ src/main/java/org/maxbot/miniapp/client/MaxApiClient.java
@@ -1,10 +1,14 @@
 package org.maxbot.miniapp.client;
 
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 import java.util.List;
 import java.util.Map;
@@ -16,8 +20,14 @@ public class MaxApiClient {
 
     public MaxApiClient(@Value("${max.api.token}") String token) {
 
+        System.out.println(">>> Max TOKEN: " + token);
+
+        HttpClient httpClient = HttpClient.create()
+                .wiretap("reactor.netty.http.client", LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
+
         this.webClient = WebClient.builder()
                 .baseUrl("https://platform-api2.max.ru")
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .defaultHeader("X-API-Key", token)
                 .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .build();
@@ -29,6 +39,8 @@ public class MaxApiClient {
                 "text", text,
                 "attachments", List.of()
         );
+
+        System.out.println(">>> Send message: " + payload);
 
         webClient.post()
                 .uri(uriBuilder -> uriBuilder
