a2fb29c9428aa608009172b34e10ad4cb7c04b4e
alex <4.alex.skorikov@gmail.com>
2026-07-13 22:44:43 +0300

    Поиск в боте 14

--- src/main/java/org/maxbot/miniapp/dto/bot/CallbackDto.java
+++ src/main/java/org/maxbot/miniapp/dto/bot/CallbackDto.java
@@ -1,5 +1,6 @@
 package org.maxbot.miniapp.dto.bot;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,8 +9,10 @@ import lombok.ToString;
 @Setter
 @ToString
 public class CallbackDto {
+    @JsonProperty("callback_id")
     private String callbackId;
     private String payload;
+    @JsonProperty("user_id")
     private int userId;
 
 }
