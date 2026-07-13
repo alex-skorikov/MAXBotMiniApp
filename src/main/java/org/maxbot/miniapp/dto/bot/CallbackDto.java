c5671ba53676a8ca65bb047fb94591a5ee906a25
alex <4.alex.skorikov@gmail.com>
2026-07-13 19:16:27 +0300

    Поиск в боте 8

--- src/main/java/org/maxbot/miniapp/dto/bot/CallbackDto.java
+++ src/main/java/org/maxbot/miniapp/dto/bot/CallbackDto.java
@@ -6,7 +6,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CallbackDto {
-    private String id;
+    private String callback_id;
     private String payload;
     private int user_id;
+
 }
+
