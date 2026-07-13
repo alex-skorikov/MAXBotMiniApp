6093a82399fbf406c98ada8b9eb5fc6e73b01160
alex <4.alex.skorikov@gmail.com>
2026-07-13 23:24:59 +0300

    Поиск в боте 17

--- src/main/java/org/maxbot/miniapp/dto/bot/BodyDto.java
+++ src/main/java/org/maxbot/miniapp/dto/bot/BodyDto.java
@@ -1,8 +1,11 @@
 package org.maxbot.miniapp.dto.bot;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,6 +14,8 @@ public class BodyDto {
     private String text;
     private String mid;
     private long seq;
-    private String payload;
+    //    private String payload;
+    @JsonIgnore
+    private List<Object> attachments;
 
 }
