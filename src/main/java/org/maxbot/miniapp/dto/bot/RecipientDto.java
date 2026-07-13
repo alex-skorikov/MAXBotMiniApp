d11e571e8c2c2b25786558a5c6cbaaf731233ea2
alex <4.alex.skorikov@gmail.com>
2026-07-13 21:52:05 +0300

    Поиск в боте 12

--- src/main/java/org/maxbot/miniapp/dto/bot/RecipientDto.java
+++ src/main/java/org/maxbot/miniapp/dto/bot/RecipientDto.java
@@ -2,9 +2,11 @@ package org.maxbot.miniapp.dto.bot;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class RecipientDto {
     private int chatId;
     private String chatType;
