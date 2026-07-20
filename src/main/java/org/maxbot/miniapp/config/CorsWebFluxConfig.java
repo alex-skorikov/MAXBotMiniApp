4493d498a2ccc98df535a5bec82f9e66a8b94982
alex <4.alex.skorikov@gmail.com>
2026-07-20 20:52:31 +0300

    Поиск в webapp cors 2

--- src/main/java/org/maxbot/miniapp/config/CorsWebFluxConfig.java
+++ src/main/java/org/maxbot/miniapp/config/CorsWebFluxConfig.java
@@ -1,6 +1,10 @@
 package org.maxbot.miniapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
@@ -16,4 +20,19 @@ public class CorsWebFluxConfig implements WebFluxConfigurer {
                 .allowCredentials(false)
                 .maxAge(3600);
     }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
 }
