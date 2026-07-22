a209b41402114c3797db63ba0c0d183138625794
alex <4.alex.skorikov@gmail.com>
2026-07-22 17:58:21 +0300

    Поиск в webapp CORS 78

--- src/main/java/org/maxbot/miniapp/config/CorsWebFluxConfig.java
+++ src/main/java/org/maxbot/miniapp/config/CorsWebFluxConfig.java
@@ -12,44 +12,44 @@ import java.util.Collections;
 @Configuration
 public class CorsWebFluxConfig {
 
-//    @Bean
-//    public CorsWebFilter corsWebFilter() {
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.addAllowedOrigin("https://max-webapp-five.vercel.app");
-//        config.addAllowedOriginPattern("*");
-//        config.addAllowedMethod("*");
-//        config.addAllowedHeader("*");
-//        config.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", config);
-//
-//        return new CorsWebFilter(source);
-//    }
-
     @Bean
     public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-
-        corsConfig.setAllowedOrigins(Arrays.asList(
-                "https://max-webapp-production-cd1a.up.railway.app",
-                "https://t.me",
-                "https://web.telegram.org",
-                "https://miniapp.maxpatent.ru"
-        ));
-
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfig.setAllowedHeaders(Collections.singletonList("*"));
-
-        corsConfig.setAllowCredentials(true); // ВАЖНО
-
-        corsConfig.setMaxAge(3600L);
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://max-webapp-production-cd1a.up.railway.app");
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
+        source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
     }
+
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//
+//        corsConfig.setAllowedOrigins(Arrays.asList(
+//                "https://max-webapp-production-cd1a.up.railway.app",
+//                "https://t.me",
+//                "https://web.telegram.org",
+//                "https://miniapp.maxpatent.ru"
+//        ));
+//
+//        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+//
+//        corsConfig.setAllowCredentials(true); // ВАЖНО
+//
+//        corsConfig.setMaxAge(3600L);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return new CorsWebFilter(source);
+//    }
 
 
 }
