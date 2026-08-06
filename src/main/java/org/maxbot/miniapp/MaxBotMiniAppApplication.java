26a0b73c69d2376062253bf16f9347562b1b19c5
alex <4.alex.skorikov@gmail.com>
2026-08-06 16:18:17 +0300

    OTHER-802 bot автодеплой 4

--- src/main/java/org/maxbot/miniapp/MaxBotMiniAppApplication.java
+++ src/main/java/org/maxbot/miniapp/MaxBotMiniAppApplication.java
@@ -1,11 +1,20 @@
 package org.maxbot.miniapp;
 
+import org.maxbot.miniapp.client.MaxApiClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class MaxBotMiniAppApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(MaxBotMiniAppApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(MaxBotMiniAppApplication.class, args);
+        log.info("BUILD VERSION: {}", LocalDateTime.now());
     }
 }
